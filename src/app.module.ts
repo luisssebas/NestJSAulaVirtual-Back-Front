022951ff54b973898905a3f78b290fc406e37dfa
luissebas <luissebasmanzano@hotmail.com>
2018-10-29 19:41:00 -0500

    http erros and logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,20 @@ import { EstudianteModule } from './estudiante/estudiante.module';
 import { MateriaModule } from './materia/materia.module';
 import { ProfesorModule } from './profesor/profesor.module';
 import { TareaModule } from './tarea/tarea.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from 'shared/http-error.filter';
+import { LoggingInterceptor } from 'shared/logging.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), CursoModule, EstudianteModule, MateriaModule, ProfesorModule, TareaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_FILTER,
+    useClass: HttpErrorFilter
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor,
+  }],
 })
 export class AppModule {}
---
+++ src/shared/http-error.filter.ts
@@ -0,0 +1,27 @@
+import { Catch, ExceptionFilter, HttpException, ArgumentsHost, Logger } from '@nestjs/common';
+
+@Catch()
+export class HttpErrorFilter implements ExceptionFilter{
+    catch(exception: HttpException, host: ArgumentsHost){
+        const ctx= host.switchToHttp();
+        const request = ctx.getRequest();
+        const response = ctx.getResponse();
+        const status = exception.getStatus();
+
+        const errorResponse = {
+            code: status,
+            timestamp: new Date().toLocaleDateString(),
+            path: request.url,
+            method: request.method,
+            message: exception.message || exception.message || null,
+        };
+
+        Logger.error(
+            '${request.method} ${request.url}',
+            JSON.stringify(errorResponse),
+            'ExceptionFilter'
+        );
+
+        response.status(status).json({found: false});
+    }
+}
