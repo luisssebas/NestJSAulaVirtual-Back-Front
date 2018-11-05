11fc34928c099487e2dc989efe5b67fbc018bb7f
luissebas <luissebasmanzano@hotmail.com>
2018-11-05 14:12:25 -0500

    create ready

--- src/curso/curso.controller.ts
+++ src/curso/curso.controller.ts
@@ -16,24 +16,42 @@ export class CursoController {
         return {cursos};
     }
 
-    @Post('create')
+    //Create GET
+
+    @Get('create')    
     @Render('Curso/create')
+    async showCursos(){
+        const  cursos = await this.cursoService.showAll();
+        return {cursos};
+    }
+
+    //Create POST
+
+    @Post('create')
     createCurso(@Body() data: CursoDTO){
         this.logger.log(JSON.stringify(data));
         return this.cursoService.create(data);
     }
 
-    @Get('data/:id')
-    readCurso(@Param('id') id: number){
-        return this.cursoService.read(id);
+    //Update GET
+
+    @Get('update/:id')    
+    @Render('Curso/update')
+    async readCurso(@Param('id') id: number){
+        const cursos = await this.cursoService.read(id);
+        return {cursos};
     }
 
-    @Put('update/:id')
-    @Render('Curso/edit')
+    //Update POST
+
+    @Post('update/:id')
     updateCurso(@Param('id') id: number, @Body() data: Partial<CursoDTO>){
         this.logger.log(JSON.stringify(data));
+        console.log(data);
         return this.cursoService.update(id, data);
     }
+
+    //Delete POST
 
     @Delete('delete/:id')
     destroyCurso(@Param('id') id:number){
